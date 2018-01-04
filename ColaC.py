741db28e7b5031b8e018d8f814c241d203b30920
Salas\Jorge Salazar <jorsala01@gmail.com>
2018-01-03 18:50:27 -0600

    primera pantalla creada

---
+++ ColaC.py
@@ -0,0 +1,1 @@
+ColaC.py
--- Spotify/src/spotify/Spotify.java
+++ Spotify/src/spotify/Spotify.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package appdesktop;
+package spotify;
 
-/**
- *
- * @author Jorge Salazar
- */
-public class AppDesktop {
+
+public class Spotify {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login log = new Login();
+        log.setVisible(true);
     }
     
 }
